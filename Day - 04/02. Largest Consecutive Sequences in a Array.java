fc1f58993a98b496359fec884657df401f5f69f0
Arnav Rangari <99029902+hashtag-longrangers@users.noreply.github.com>
2023-09-21 20:50:02 +0530

    Create 02. Largest Consecutive Sequences in a Array.java

---
+++ Day - 04/02. Largest Consecutive Sequences in a Array.java
@@ -0,0 +1,29 @@
+https://leetcode.com/problems/longest-consecutive-sequence/
+
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        if ( nums.length == 0 ) {
+            return 0;
+        }
+        
+        Arrays.sort(nums);
+        int count = 1;
+        int maxCount = 1;
+
+        for( int i = 0; i < nums.length - 1; i++ ) {
+            if ( nums[i + 1] == nums[i] + 1 ) {
+                count++;
+            }
+            
+            else if ( nums[i+1] == nums[i]){
+                continue;
+            }
+            else {
+                count = 1;
+            }
+            
+            maxCount = Math.max(count, maxCount);
+        }
+        return maxCount;
+    }
+}
