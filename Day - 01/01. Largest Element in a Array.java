c5de2e0d5769be5f8cf8562483d369521f45dbee
Arnav Rangari <99029902+hashtag-longrangers@users.noreply.github.com>
2023-09-30 16:10:19 +0530

    Create 01. Largest Element in a Array.java

---
+++ Day - 01/01. Largest Element in a Array.java
@@ -0,0 +1,18 @@
+import java.util.* ;
+import java.io.*; 
+
+public class Solution {
+
+    static int largestElement(int[] arr, int n) {
+        // Write your code here.
+        int max = Integer.MIN_VALUE;
+
+        for ( int i = 0; i < arr.length; i++ ) {
+            if ( max < arr[i] ) {
+                max = arr[i];
+            }
+        }
+        return max;
+
+    }
+}
